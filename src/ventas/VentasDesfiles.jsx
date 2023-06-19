31c0c1a8d78b44eed12d4a981e051dd77239c424
Maria Paola Galvis <marugalvis04@gmail.com>
2023-06-19 10:33:19 -0400

    add: cambios ventas

--- src/ventas/VentasDesfiles.jsx
+++ src/ventas/VentasDesfiles.jsx
@@ -1,6 +1,37 @@
 
 export const VentasDesfiles = () => {
   return (
-    <h1>VentasDesfiles</h1>
+    <div className="container pt-4">
+        <table class="table caption-top">
+      <thead class="table-success">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Empresa</th>
+          <th scope="col">Tipo Entrada</th>
+          <th scope="col">Cantidad Maxima</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+    
   )
 }
--- src/ventas/VentasPage.jsx
+++ src/ventas/VentasPage.jsx
@@ -15,35 +15,36 @@ export const VentasPage = () => {
     return (
 
     <>
-        <div className="card">
+       <div class="card w-75 mb-4 mt-4 mx-auto">
+       <div class="row g-0">
+           <div class="col-md-4">
+            <img src="https://img.freepik.com/foto-gratis/dos-entradas-vista-frontal-azul-aislado-blanco_1101-3055.jpg?w=996&t=st=1687179903~exp=1687180503~hmac=36a16ceec8cf16be5ce7da2731940044a0cfebf4e85fca1ba7eea78776b71414" class="img-fluid rounded-start" alt="..."/>
+        </div>
+        <div class="col-md-8">
+           <div class="card-body">
+             <h5 class="card-title"><strong>Venta De Entradas - Desfiles</strong></h5>
+             <p class="card-text">El siguiente apartado es utilizado para todo lo que tiene que ver con la venta de entradas de los defiles. </p>
+             <button className="btn btn-success" onClick={toDesfiles}> Seleccionar </button>
+           </div>
+        </div>
+       </div>
+    </div>
 
-        <div className="card-body">
-            <h5 className="card-title">Ventas de entradas para desfiles</h5>
-            <p className="card-text">
-                Este apartado sera utilizado para todo lo que tiene que ver con los 
-                eventos que suceden en el sambodromo los cuales son los desfiles.</p>
-            <button 
-            className="btn btn-primary"
-            onClick={toDesfiles}
-            >Seleccionar</button>
+    <div class="card w-75 mb-4 mt-4 mx-auto">
+       <div class="row g-0">
+           <div class="col-md-4">
+            <img src="https://img.freepik.com/foto-gratis/rosa-admitir-entradas-aisladas-blanco_1101-3056.jpg?w=996&t=st=1687179912~exp=1687180512~hmac=3bbeead74c493360d69ae2e850aee9dc981ba56083ff55a592582457fa3ac99d" class="img-fluid rounded-start" alt="..."/>
         </div>
-        <hr />
+        <div class="col-md-8">
+           <div class="card-body">
+             <h5 class="card-title"><strong>Venta De Entradas - Eventos Generales</strong></h5>
+             <p class="card-text">El siguiente apartado es utilizado para todo lo que tiene que ver con la venta de entradas de los eventos generales. </p>
+             <button className="btn btn-success" onClick={toGenerales}> Seleccionar </button>
+           </div>
         </div>
+       </div>
+    </div>
 
-        <div className="card">
-
-        <div className="card-body">
-            <h5 className="card-title">Venta de entradas para eventos generales</h5>
-            <p className="card-text">
-                Este apartado sera utilizado para todo lo que tiene que ver 
-                con las venta de entradas a eventos generales.</p>
-            <button  
-            className="btn btn-primary"
-            onClick={toGenerales}
-            >Seleccionar</button>
-        </div>
-        <hr />
-        </div>
     </>
   )
 }
