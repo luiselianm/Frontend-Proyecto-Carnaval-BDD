92487d486d80421062d37c026964fc6000e3285b
Luis Elian <luiselian001@gmail.com>
2023-06-19 01:25:24 -0400

    Lista de eventos se muestra pero no funciona bien

--- src/planificacion/EventList.jsx
+++ src/planificacion/EventList.jsx
@@ -1,49 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const EventList = () => {
-  return (
-    <>
-        <div className="container">
 
-        <div class="accordion accordion-flush" id="accordionFlushExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                Primer evento
-            </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body">
-                Descripcion del primer evento
+    const [ eventos, setEventos] = useState([]);
+
+    const getEventos = async () =>{
+        try {
+            
+            const response = await fetch("http://localhost:5000/eventos");
+            const jsonData = await response.json();
+
+            console.log(jsonData);
+
+            setEventos(jsonData);
+
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getEventos();
+    },[])
+
+
+    return (
+        <>
+            <div className="container">
+
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                {eventos.map(evento => (
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                {evento.nombre} 
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                {evento.descripcion}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                </div>
+                    
             </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                Segundo evento
-            </button>
-            </h2>
-            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body">
-                Descripcion del segundo evento evento
-            </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                Tercer evento
-            </button>
-            </h2>
-            <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body">
-                Descripcion del tercer evento
-            </div>
-            </div>
-        </div>
-        </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
